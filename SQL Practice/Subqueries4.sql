932499b51378ad1b1c6216407e3b82c28a324bbd
Vijay Krishnamoorthy <64549719+vijayk0747@users.noreply.github.com>
2020-12-15 17:25:27 -0600

    Delete Subqueries4.sql

--- SQL Practice/Subqueries4.sql
+++
@@ -1,9 +0,0 @@
---Create a correlated subquery to list out all countries having more than 8 events.
-
-SELECT tc.CountryName
-
-FROM tblCountry tc 
-
-WHERE 8 < (SELECT COUNT(te.EventID) FROM tblEvent te  WHERE tc.CountryID = te.CountryID)
-
-GROUP BY tc.CountryName
